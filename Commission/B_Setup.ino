f95a986623bbc008799c98df2fd5830e97054137
Pat <mediodia.patrick@gmail.com>
2023-11-16 13:48:35 +0800

    Moved setting of relay values before task startup

--- Commission/B_Setup.ino
+++ Commission/B_Setup.ino
@@ -7,6 +7,10 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
+  //initiate emitter and fan filter as off
+  digitalWrite(OzoneEmitter, HIGH);
+  digitalWrite(CarbonFilter, HIGH);
+
   //Initialize Ozone Sensor I2C Interface
   while(!Ozone.begin(Ozone_IICAddress)) {
     Serial.println("I2c device number error !");
@@ -21,15 +25,11 @@ void setup() {
   //startup tasks
   xTaskCreate(T_CheckOzone, "Sensor", 128, NULL, 1, &H_CheckOzone);
   vTaskSuspend(H_CheckOzone);
-  
+
   xTaskCreate(T_Runtime, "Timer", 140, NULL, 1, &H_Runtime);
   vTaskSuspend(H_Runtime);
 
   xTaskCreate(T_CheckRunningStatus, "Buttons", 96, NULL, 1, &H_CheckRunningStatus);
-
-  //initiate emitter and fan filter as off
-  digitalWrite(OzoneEmitter, HIGH);
-  digitalWrite(CarbonFilter, HIGH);
 
   displayStartMessage();
 }
